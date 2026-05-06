1e478266bc9bd3b68e54b3744c9efacfc379940b
forky <agilanakbaik621@gmail.com>
2026-05-06 18:02:50 +0700

    Create loaderfree.lua

---
+++ loaderfree.lua
@@ -0,0 +1,41 @@
+repeat task.wait() until game:IsLoaded()
+
+_G.ForkyHUB = _G.ForkyHUB or {}
+
+_G.ForkyHUB.Games = {
+    [2693023319] = {
+        name = "Expedition Antarctica",
+        url = "https://gitlab.com/forky1/forkyHUB/-/raw/main/artic.lua"
+    },
+    [129118369937980] = {
+        name = "Eagle Nation",
+        url = "https://gitlab.com/forky1/forkyHUB/-/raw/main/eagle.lua"
+    },
+    [129866685202296] = {
+        name = "Last Letter",
+        url = "https://gitlab.com/forky1/forkyHUB/-/raw/main/lastletter.lua"
+    },
+    [131378148336503] = {
+        name = "Drag Drive Simulator",
+        url = "https://gitlab.com/forky1/forkyHUB/-/raw/main/ddds.lua"
+    },
+    [77747658251236] = {
+        name = "Sailor Piece",
+        url = "https://raw.githubusercontent.com/forky-y/UI/refs/heads/main/ssl.lua"
+    },
+    [999999999] = {
+        name = "Fishzar",
+        url = ""
+    }
+}
+
+function _G.ForkyHUB.Load(url)
+    return loadstring(game:HttpGet(url))()
+end
+
+local gameData = _G.ForkyHUB.Games[game.PlaceId]
+
+local url = gameData and gameData.url
+    or "https://gitlab.com/forky1/forkyHUB/-/raw/main/mts.lua"
+
+_G.ForkyHUB.Load(url)
