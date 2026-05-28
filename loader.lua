49b463f8bc66a8e65f3a439ce5f865a4af09243e
forky <agilanakbaik621@gmail.com>
2026-05-28 17:04:41 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -33,11 +33,11 @@ _G.ForkyHUB.Games = {
     },
     [110369730911937] = {
         name = "East JavaCDID",
-        url = "https://pastefy.app/gPnP6gEh/raw"
+        url = "https://pastefy.app/0JJgTSPp/raw"
     },
     [9508940498] = {
         name = "Central Java CDID",
-        url = "https://pastefy.app/gPnP6gEh/raw"
+        url = "https://pastefy.app/0JJgTSPp/raw"
     },
     [132986577553100] = {
         name = "Seasonal CDID",
